2431f1db6ed6e4613e2a72037dda1aad687a80bf
99Page <nou0ggid@gmail.com>
2023-02-12 15:23:08 +0900

    union find

---
+++ Algorithm/Algorithm/UnionFind.swift
@@ -0,0 +1,44 @@
+//
+//  UnionFind.swift
+//  Algorithm
+//
+//  Created by 노우영 on 2023/02/12.
+//  UnionFind, 유니온파인드 
+
+import Foundation
+
+let size = 10000
+var parents: [Int] = [Int]()
+var groupSize: [Int] = [Int]()
+
+func findRoot(_ u: Int) -> Int {
+    if (parents[u] == u) {
+        return u
+    }
+    
+    parents[u] = findRoot(parents[u])
+    return parents[u]
+}
+
+func unionGroup(_ u: Int, _ v: Int) {
+    let rootU: Int = findRoot(u)
+    let rootV: Int = findRoot(v)
+    
+    if groupSize[rootU] < groupSize[rootV] {
+        parents[rootU] = rootV
+        groupSize[rootV] += groupSize[rootU]
+    } else {
+        parents[rootV] = rootU;
+        groupSize[rootU] += groupSize[rootV];
+    }
+}
+
+func isSameGroup(_ u: Int, _ v: Int) -> Bool {
+    return findRoot(u) == findRoot(v)
+}
+
+func initParent() {
+    for i in 0..<size {
+        parents[i] = i
+    }
+}
